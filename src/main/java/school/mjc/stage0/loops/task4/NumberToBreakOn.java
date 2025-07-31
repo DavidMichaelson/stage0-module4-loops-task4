529766656dc41586169fc841b58dc7112c5e2e37
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-07-31 11:50:06 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,5 +3,29 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
+        if (numberToGoUntil < toBreakWith) {
+            System.out.println("iterating till the end");
+
+            for (int i = 0; i <= numberToGoUntil; i++) {
+                System.out.println(i);
+                }
+
+        } else {
+
+            for (int j = 0; j <= toBreakWith; j++) {
+                System.out.println(j);
+                if (j == toBreakWith) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberToBreakOn numberToBreakOn = new NumberToBreakOn();
+
+        numberToBreakOn.printNumbersUntilWithBreakOn(10, 9);
+        numberToBreakOn.printNumbersUntilWithBreakOn(8, 10);
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.loops.task4;
 
+import javax.swing.plaf.SliderUI;
+
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int count = 0;
+
+        for (int i = 1; i <= lastPrinted; i++) {
+            count++;
+
+            if (count % 3 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        SkippingThird skippingThird = new SkippingThird();
+        skippingThird.printUntilButThird(30);
     }
 }
